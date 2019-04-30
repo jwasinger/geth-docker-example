74c794436a01ea1ef1bdea66a5af6b11b8ee0a2a
Jared Wasinger <j-wasinger@hotmail.com>
2019-04-30 22:37:03 +0000

    cleanup

--- init-geth.sh
+++ init-geth.sh
@@ -1,7 +1,4 @@
 #! /bin/bash
-mkdir -p data
+mkdir data
 geth init ./genesis.json --datadir ./data
 geth --datadir ./data account import ./keys/faucet/faucet-priv.txt --password ./keys/faucet/faucet-pw.txt
-# docker run -v $(pwd)/keys:/keys -v $(pwd)/data:/data -t ethereum/client-go --datadir /data account import /keys/deployer/deployer-priv.txt --password /keys/deployer/deployer-pw.txt
-# docker run -v $(pwd)/data:/data -t ethereum/client-go init genesis.json --datadir /data
-# ./geth account import 
