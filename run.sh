c972195079f4fa6ec7e1b9347385ddd1aac53a05
Jared Wasinger <j-wasinger@hotmail.com>
2019-05-03 22:54:32 +0000

    connect to geth over RPC instead of IPC

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
 #! /bin/bash
 
-geth --etherbase $(cat keys/faucet/faucet-addr.txt) --mine --miner.threads 1 --networkid 66  --vmodule "miner=12" --datadir ./data --unlock $(cat keys/faucet/faucet-addr.txt) --password keys/faucet/faucet-pw.txt
+geth --etherbase $(cat keys/faucet/faucet-addr.txt) --mine --miner.threads 1 --networkid 66  --vmodule "miner=12" --datadir ./data --unlock $(cat keys/faucet/faucet-addr.txt) --password keys/faucet/faucet-pw.txt \
+    --rpc \
+    --rpcaddr '0.0.0.0' \
+    --rpcport 8545 \
+    --rpccorsdomain '*' \
+    --ws \
+    --wsaddr '0.0.0.0' \
+    --wsorigins '*' \
+    --nodiscover \
+    --vm.ewasm="$(pwd)/libhera.so,fallback=true,metering=false"
