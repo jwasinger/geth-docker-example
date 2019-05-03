571a87fe7ff7d10be6fdf71d1f7772aeb70f55d9
Jared Wasinger <j-wasinger@hotmail.com>
2019-05-03 22:55:04 +0000

    more

--- metacoin/truffle-config.js
+++ metacoin/truffle-config.js
@@ -4,9 +4,13 @@ var net = require("net");
 module.exports = {
   networks: {
     dev: {
+	    /*
       provider: function() {
         return new Web3.providers.IpcProvider("/home/ubuntu/projects/geth-example/data/geth.ipc", net);
       },
+      */
+      host: "127.0.0.1",
+      port: 8545,
       network_id: '66',
       from: '7976977ecf72519e656a27c16b8c406329e46b78'
     }
